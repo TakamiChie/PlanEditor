4225d3c6ff0faae71587d50bae4d66564c5530a9
TakamiChie@Ishtar <chie@onpu-tamago.net>
2018-01-15 13:29:45 +0900

    #5 対応

--- src/index.js
+++ src/index.js
@@ -1,23 +1,41 @@
+const ROLE = {
+  CHAPTER: "CHAPTER",
+  AGGREGATE: "AGGREGATE",
+  CHARGE: "CHARGE",
+  TEXT: "TEXT",
+  NUMBER: "NUMBER",
+  DATE: "DATE",
+  STATIC: "STATIC"
+}
+
+function createCell({
+  rowobject, 
+  insertIndex = -1,
+  role = ROLE.STATIC,
+  value = "",
+} = {}){
+  if(rowobject == undefined){
+    throw new Error("Argument `rowobject` is not defined");
+  }
+  let cell = rowobject.insertCell(insertIndex);
+  if(role == ROLE.STATIC){
+    // セルはラベル
+    cell.contentEditable = false;
+  }else{
+    // セルは編集可能
+    cell.contentEditable = true;
+    cell.className = "editable";
+  }
+  cell.dataset.role = role;
+  cell.textContent = value;
+}
+
 document.querySelector("#appendrow").addEventListener("click", () =>{
   let editorui = document.querySelector("#editorui");
   let row = editorui.insertRow(editorui.rows.length - 1);
-  let cNum = row.insertCell(-1);
-  cNum.innerHTML = "0-0-0-0";
-
-  let cTitle = row.insertCell(-1);
-  cTitle.contentEditable = true;
-  cTitle.className ="editable";
-  cTitle.innerHTML = "test";
-  
-  let cVolume = row.insertCell(-1);
-  cVolume.contentEditable = true;
-  cVolume.className ="editable";
-  cVolume.innerHTML = "0";
-  
-  let cDescription = row.insertCell(-1);
-  cDescription.contentEditable = true;
-  cDescription.className ="editable";
-  cDescription.innerHTML = "test";
-  
-  let cGraph = row.insertCell(-1);
+  createCell({rowobject: row, value: "0"});
+  createCell({rowobject: row, role: ROLE.CHAPTER, value: "test"});
+  createCell({rowobject: row, role: ROLE.AGGREGATE, value: "0"});
+  createCell({rowobject: row, role: ROLE.TEXT});
+  createCell({rowobject: row});
 });
