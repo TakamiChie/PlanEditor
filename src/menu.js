1d6c19659880176b809f7d4c68245f7e5adc5898
TakamiChie@Ishtar <chie@onpu-tamago.net>
2018-01-27 10:18:00 +0900

    モディファイアキー実装

--- src/menu.js
+++ src/menu.js
@@ -1,22 +1,22 @@
 // ref:https://github.com/theodi/comma-chameleon/blob/master/main/menu.js
 exports.menu = [
   {
-    label: "ファイル",
+    label: "ファイル(&F)",
   },
   {
-    label: "計画",
+    label: "計画(&P)",
     submenu: [
       {
-        label: "列の追加",
+        label: "列の追加(&C)",
         click: function() { BrowserWindow.getFocusedWindow().webContents.send("appendColumn"); }
       }
     ]
   },
   {
-    label: "ヘルプ",
+    label: "ヘルプ(&H)",
     submenu: [
       {
-        label: "開発者ツール",
+        label: "開発者ツール(&D)",
         accelerator: "CmdOrCtrl+Shift+I",
         click: function() { BrowserWindow.getFocusedWindow().toggleDevTools(); }
       }
