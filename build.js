ae118c02f467b8dd14ed8c26817c943df613bd4f
TakamiChie@Visnu <chie@onpu-tamago.net>
2018-04-19 15:08:59 +0900

    ビルドスクリプト作成

---
+++ build.js
@@ -0,0 +1,29 @@
+const packager = require("electron-packager");
+// 毎回オプションを書き直すのは面倒くさいのでpackage.jsonから引っ張ってくる
+const package = require("./package.json");
+
+packager({
+    name: package["name"],
+    dir: ".",// ソースフォルダのパス
+    out: "./dist",// 出力先フォルダのパス
+    icon: "./icon/appicon.ico",// アイコンのパス
+    platform: "win32",
+    arch: "x64",
+    version: "1.0.1",// Electronのバージョン
+    overwrite: true,// 上書き
+    asar: false,// asarパッケージ化
+    "app-version": package["version"],// アプリバージョン
+    "app-copyright": "Copyright (C) 2018 "+package["author"]+".",// コピーライト
+    
+    "version-string": {// Windowsのみのオプション
+        CompanyName: "横浜IT勉強会",
+        FileDescription: package["name"],
+        OriginalFilename: package["name"]+".exe",
+        ProductName: package["name"],
+        InternalName: package["name"]
+    }
+    
+}, function (err, appPaths) {// 完了時のコールバック
+    if (err) console.log(err);
+    console.log("Done: " + appPaths);
+});
