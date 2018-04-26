5d7ccffac0a10c8ab70b798ce55d117d91a3df33
TakamiChie@Visnu <chie@onpu-tamago.net>
2018-04-26 13:50:20 +0900

    実行可能なexeを作れるように変更

--- build.js
+++ build.js
@@ -1,28 +1,32 @@
 const packager = require("electron-packager");
+const exec = require('child_process').exec;
+const path = require('path');
+
 // 毎回オプションを書き直すのは面倒くさいのでpackage.jsonから引っ張ってくる
 const package = require("./package.json");
-
+let exeName = "pedit"
 packager({
     dir: ".",
-    name: package["name"],
+    name: "PlanEditor",
     appCopyright: `Copyright (C) 2018 ${package["author"]}.`,
     appVersion: package["version"],
     arch: "x64",
     aser: true,
-    executableName: "pedit",
+    executableName: exeName,
     icon: "./icon/appicon.ico",
     out: "./dist",
     overwrite: true,
     platform: "win32",
     versionString: {
         CompanyName: "横浜IT勉強会",
-        FileDescription: package["name"],
-        OriginalFilename: "pedit.exe",
-        ProductName: package["name"],
+        FileDescription: package["description"],
+        OriginalFilename: `${exeName}.exe`,
+        ProductName: "PlanEditor",
         InternalName: "WDChamomile"
     }
     
 }, function (err, appPaths) {// 完了時のコールバック
     if (err) console.log(err);
     console.log("Done: " + appPaths);
+    exec(path.join("" + appPaths, exeName + ".exe"));
 });
